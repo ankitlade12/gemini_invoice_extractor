50c5de683b9d00b519dc9a26b2d64972f384ba25
ankitlade12 <ankitlade12@gmail.com>
2024-01-08 17:23:19 -0500

    inital commit

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from dotenv import load_dotenev
+from dotenv import load_dotenv
 
-
-# load the environment variables from .env
-load_dotenev ()
+load_dotenv()  
 
 import streamlit as st
 import os
+import pathlib
+import textwrap
 from PIL import Image
+
+
 import google.generativeai as genai
 
+
+os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# function to laod Gemini Pro
-model=genai.GenerativeModel('gemini-pro-vision')
+# function to load model and get responses
 
 def get_gemini_response(input,image,prompt):
-    response=model.generate_content([input,image[0],prompt])
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+    
 
 def input_image_setup(uploaded_file):
     # Check if a file has been uploaded
@@ -32,21 +38,22 @@ def input_image_setup(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
-    
-# Stremalit initialization
-    
-st.set_page_config(page_title="Multilanguage Invoice Extractor")
+
+
+# Streamlit app initaialization
+
+st.set_page_config(page_title="Gemini Image Demo")
 
 st.header("Gemini Application")
 input=st.text_input("Input Prompt: ",key="input")
-uploaded_file = st.file_uploader("Select an image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image=""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
 
-submit=st.button("Tell me about the Invoice")
+submit=st.button("Tell me about the image")
 
 input_prompt = """
                You are an expert in understanding invoices.
@@ -54,7 +61,6 @@ input_prompt = """
                you will have to answer questions based on the input image
                """
 
-## If ask button is clicked
 
 if submit:
     image_data = input_image_setup(uploaded_file)
